1e1275f6baaed7031f23b996f7ac7b7af9213889
watermandrew <Drew.C.Waterman.18@Dartmouth.edu>
2017-05-13 16:57:47 -0400

    adding db with firebase; can insert simple category into db

--- src/components/new_category.js
+++ src/components/new_category.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import * as firebasedb from '../firebasedb';
 
 class NewCategory extends Component {
   constructor(props) {
@@ -7,15 +7,33 @@ class NewCategory extends Component {
 
     this.state = {
       title: '',
-      links: [],
+      links: this.props.links,
+      selectedLinks: [],
     };
     this.onInputChange = this.onInputChange.bind(this);
+    this.createCategory = this.createCategory.bind(this);
     // this.onButtonClick = this.onButtonClick.bind(this);
   }
-
+  // componentDidMount() {
+  //   this.setState({ links: firebasedb.fetchAllLinks() });
+  // }
   onInputChange(event) {
     this.setState({ title: event.target.value });
   }
+  onButtonClick(selected) {
+    const newArray = this.state.selectedLinks;
+    newArray.push(selected);
+    this.setState({ selectedLinks: newArray });
+  }
+  createCategory() {
+    const cat = {
+      title: this.state.title,
+      links: this.state.selectedLinks,
+    };
+
+    firebasedb.createCategory(cat);
+  }
+
 
   // http://stackoverflow.com/questions/26505064/react-js-what-is-the-best-way-to-add-a-value-to-an-array-in-state
   // onButtonClick(event) {
@@ -30,12 +48,13 @@ class NewCategory extends Component {
       <div>
         <div>New Category</div>
         <div>Title: <input onChange={this.onInputChange} value={this.state.title} /></div>
-        {this.props.links.map((link) => {
+        {this.state.links.map((link) => {
           console.log(link);
           return (
-            <div>{link}<button onClick={this.onButtonClick}>Add</button></div>
+            <div>{link}<button onClick={clicked => this.onButtonClick(link)}>Add</button></div>
           );
         })}
+        <button onClick={this.createCategory}>Create</button>
       </div>
     );
   }

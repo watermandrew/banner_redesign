9d60ad1c6ee9fadf5d9d9f7f85eba71fb4316498
watermandrew <Drew.C.Waterman.18@Dartmouth.edu>
2017-05-13 15:04:29 -0400

    simple category component; need to implement adding links, but removing works

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Category from './components/category';
+
+import './style.scss';
 
 const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
+  return (
+    <div>
+      <Category />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ['babel-loader'],
       },
     ],
   },

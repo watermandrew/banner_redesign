32bbb7c0644d1d07393ec43883bb9ae20ba25529
Mahnoor Maqsood <mahnoor.maqsood@gmail.com>
2017-05-13 01:03:09 -0400

    added react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const $ = require('jquery');
+const App = () => {
+  return <div className="test">All the React are belong to us!</div>;
+};
 
-$('#main').html('done!!');
+ReactDOM.render(<App />, document.getElementById('main'));
